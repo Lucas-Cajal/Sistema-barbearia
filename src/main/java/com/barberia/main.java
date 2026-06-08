5127cf613f3aa47eab75c83cd1d0cf73ed4ed298
Lucas-Cajal <lucasandradecajal17@gmail.com>
2026-06-08 18:34:14 -0300

    feature: inicialize project with basic structure and gitignore

---
+++ src/main/java/com/barberia/main.java
@@ -0,0 +1,14 @@
+package com.barberia;
+
+public class main {
+
+    /**
+     * barbershop scheduling system
+     * @author Lucas Cajal
+     * version 1.0
+     */
+
+    public static void main(String[] args){
+            System.out.println("Barbershop system STARTED!");
+        }
+}
